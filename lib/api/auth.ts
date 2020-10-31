2e880a9866c00767f192b9ed1596e69c2af95769
sk97kang <sk97.kang@gmail.com>
2020-10-31 15:26:47 +0900

    Add Login

---
+++ lib/api/auth.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { APIResponse } from "../types/api";
+
+const api = axios.create({
+  baseURL: "http://3.214.168.45:8080/api/v1",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/hal+json",
+  },
+});
+
+export const onLogin = async (email: string, password: string) => {
+  try {
+    const data = {
+      email,
+      password,
+    };
+    const res = await api.post<APIResponse>(`/auth/login`, data);
+    return [res.data, null];
+  } catch (err) {
+    return [null, err.response.data];
+  }
+};
--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -1,7 +1,89 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { useRouter } from "next/router";
+
+import styled from "styled-components";
+import {
+  Form,
+  Typography,
+  Input,
+  Checkbox,
+  Button,
+  Divider,
+  message,
+} from "antd";
+import useInput from "../../../hooks/useInput";
+import { onLogin } from "../../../lib/api/auth";
+
+const { Text } = Typography;
 
 function IndexPage() {
-  return <div>Login</div>;
+  const router = useRouter();
+
+  const [loginLoading, setLoginLoading] = useState(false);
+  const [email, onChangeEmail] = useInput("");
+  const [password, onChangePassword] = useInput("");
+
+  const onSubmit = useCallback(async () => {
+    setLoginLoading(true);
+    const [data, error] = await onLogin(email, password);
+    console.log(data, error);
+    setLoginLoading(false);
+    if (data) {
+      message.success("로그인에 성공하였습니다.");
+      setTimeout(() => {
+        router.push("/");
+      }, 1000);
+    } else {
+      message.error(error.message);
+    }
+  }, [email, password]);
+
+  return (
+    <Form onFinish={onSubmit}>
+      <div>
+        <Text>로그인하기</Text>
+        <br />
+        <Text>본인인증을 위한 인증번호가 발송됩니다.</Text>
+      </div>
+      <div>
+        <label>이메일</label>
+        <br />
+        <Input type="email" required value={email} onChange={onChangeEmail} />
+      </div>
+      <div>
+        <label>비밀번호</label>
+        <br />
+        <Input
+          type="password"
+          required
+          value={password}
+          onChange={onChangePassword}
+        />
+      </div>
+      <div>
+        <Checkbox />
+        <Text>자동 로그인</Text>
+      </div>
+      <div>
+        <Button htmlType="submit" loading={loginLoading}>
+          로그인하기
+        </Button>
+      </div>
+      <div>
+        <Text>비밀번호를 잊어버렸나요?</Text>
+      </div>
+      <Divider plain>소셜 계정으로 로그인하기</Divider>
+      <div>
+        <Button>Naver</Button>
+        <Button>Kakao</Button>
+        <Button>Google</Button>
+      </div>
+    </Form>
+  );
 }
 
 export default IndexPage;
+
+const FatText = styled.div``;
+
+const SubText = styled.div``;
